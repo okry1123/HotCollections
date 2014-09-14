549e8c5769ac48fa8632e48c558f058701128574
demonzym <demonzym@163.com>
2014-09-14 22:20:05 +0800

    设置分享信息  appid key等等

--- app/src/main/java/com/threedroid/hotcollections/fragment/FragmentGame.java
+++ app/src/main/java/com/threedroid/hotcollections/fragment/FragmentGame.java
@@ -26,6 +26,7 @@ import com.threedroid.hotcollections.onekeyshare.OnekeyShare;
 import com.threedroid.hotcollections.util.Util;
 
 import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.renren.Renren;
 import cn.sharesdk.tencent.qq.QQ;
 import cn.sharesdk.wechat.favorite.WechatFavorite;
 
@@ -186,6 +187,7 @@ public class FragmentGame extends android.support.v4.app.Fragment {
         ShareSDK.initSDK(getActivity());
         OnekeyShare oks = new OnekeyShare();
         oks.addHiddenPlatform(WechatFavorite.NAME);
+        oks.addHiddenPlatform(Renren.NAME);
 //        oks.setViewToShare(mWebView);
         //关闭sso授权
 //        oks.disableSSOWhenAuthorize();
@@ -195,19 +197,19 @@ public class FragmentGame extends android.support.v4.app.Fragment {
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
         oks.setTitle(getString(R.string.share));
         // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-//        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl("http://pan.baidu.com/s/1dD8GotN");
         // text是分享文本，所有平台都需要这个字段
         oks.setText("" + mTitle);
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         oks.setImagePath(MyApplication.getAppContext().getExternalCacheDir().getAbsolutePath() + "liulanqijietu.png");
         // url仅在微信（包括好友和朋友圈）中使用
-//        oks.setUrl("http://sharesdk.cn");
+        oks.setUrl("http://pan.baidu.com/s/1dD8GotN");
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
 //        oks.setComment("我是测试评论文本");
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
+        oks.setSiteUrl("http://pan.baidu.com/s/1dD8GotN");
 
         // 启动分享GUI
         oks.show(getActivity());
